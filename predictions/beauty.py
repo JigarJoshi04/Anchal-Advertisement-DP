959cb87f976ed1649a78fe044b84f06ceb096908
JigarJoshi <jigar.pj@somaiya.edu>
2022-03-28 17:59:41 +0530

    add: models added

--- predictions/beauty.py
+++ predictions/beauty.py
@@ -1,9 +1,20 @@
 import pickle
 from joblib import load
+import numpy as np
 
 print("Models are getting initiated: Beauty")
-phone_model_scaler = load("models/electronics_model/phone_model/std_scaler.bin")
-phone_model = pickle.load(open("models/electronics_model/phone_model/model", "rb"))
+bodycare_model_scaler = load("models/beauty_model/bodycare_model/bodycare.pkl")
+bodycare_model = pickle.load(
+    open("models/beauty_model/bodycare_model/bodycare.sav", "rb")
+)
+
+haircare_model_scaler = load("models/beauty_model/haircare_model/haircare.pkl")
+haircare_model = pickle.load(
+    open("models/beauty_model/haircare_model/haircare.sav", "rb")
+)
+
+makeup_model_scaler = load("models/beauty_model/makeup_model/makeup.pkl")
+makeup_model = pickle.load(open("models/beauty_model/makeup_model/makeup.sav", "rb"))
 print("Models are loaded succesfully : Beauty")
 
 
@@ -25,20 +36,21 @@ class BeautyPredicter:
         if not self.proper_data():
             return 100.00
 
-        scaled_data = phone_model_scaler.transform(
-            [
+        scaled_data = makeup_model_scaler.transform(
+            np.array(
                 [
-                    self.data.get("gender"),
                     self.data.get("age"),
-                    self.data.get("income"),
+                    self.data.get("income") // 1000,
+                    self.data.get("gender"),
+                    self.data.get("education"),
                 ]
-            ]
+            ).reshape(1, -1)
         )
 
         prediction = {}
-        prediction["binary"] = str(phone_model.predict(scaled_data)[0])
+        prediction["binary"] = str(makeup_model.predict(scaled_data)[0])
         prediction["probability"] = str(
-            int(max(phone_model.predict_proba(scaled_data)[0]))
+            int(max(makeup_model.predict_proba(scaled_data)[0]))
         )
         return prediction
 
@@ -46,20 +58,21 @@ class BeautyPredicter:
         if not self.proper_data():
             return 100.00
 
-        scaled_data = phone_model_scaler.transform(
-            [
+        scaled_data = makeup_model_scaler.transform(
+            np.array(
                 [
-                    self.data.get("gender"),
                     self.data.get("age"),
-                    self.data.get("income"),
+                    self.data.get("income") // 1000,
+                    self.data.get("gender"),
+                    self.data.get("education"),
                 ]
-            ]
+            ).reshape(1, -1)
         )
 
         prediction = {}
-        prediction["binary"] = str(phone_model.predict(scaled_data)[0])
+        prediction["binary"] = str(makeup_model.predict(scaled_data)[0])
         prediction["probability"] = str(
-            int(max(phone_model.predict_proba(scaled_data)[0]))
+            int(max(makeup_model.predict_proba(scaled_data)[0]))
         )
         return prediction
 
@@ -67,20 +80,21 @@ class BeautyPredicter:
         if not self.proper_data():
             return 100.00
 
-        scaled_data = phone_model_scaler.transform(
-            [
+        scaled_data = haircare_model_scaler.transform(
+            np.array(
                 [
-                    self.data.get("gender"),
                     self.data.get("age"),
-                    self.data.get("income"),
+                    self.data.get("income") // 1000,
+                    self.data.get("gender"),
+                    self.data.get("education"),
                 ]
-            ]
+            ).reshape(1, -1)
         )
 
         prediction = {}
-        prediction["binary"] = str(phone_model.predict(scaled_data)[0])
+        prediction["binary"] = str(haircare_model.predict(scaled_data)[0])
         prediction["probability"] = str(
-            int(max(phone_model.predict_proba(scaled_data)[0]))
+            int(max(haircare_model.predict_proba(scaled_data)[0]))
         )
         return prediction
 
@@ -88,20 +102,21 @@ class BeautyPredicter:
         if not self.proper_data():
             return 100.00
 
-        scaled_data = phone_model_scaler.transform(
-            [
+        scaled_data = haircare_model_scaler.transform(
+            np.array(
                 [
-                    self.data.get("gender"),
                     self.data.get("age"),
-                    self.data.get("income"),
+                    self.data.get("income") // 1000,
+                    self.data.get("gender"),
+                    self.data.get("education"),
                 ]
-            ]
+            ).reshape(1, -1)
         )
 
         prediction = {}
-        prediction["binary"] = str(phone_model.predict(scaled_data)[0])
+        prediction["binary"] = str(haircare_model.predict(scaled_data)[0])
         prediction["probability"] = str(
-            int(max(phone_model.predict_proba(scaled_data)[0]))
+            int(max(haircare_model.predict_proba(scaled_data)[0]))
         )
         return prediction
 
@@ -109,20 +124,21 @@ class BeautyPredicter:
         if not self.proper_data():
             return 100.00
 
-        scaled_data = phone_model_scaler.transform(
-            [
+        scaled_data = bodycare_model_scaler.transform(
+            np.array(
                 [
-                    self.data.get("gender"),
                     self.data.get("age"),
-                    self.data.get("income"),
+                    self.data.get("income") // 1000,
+                    self.data.get("gender"),
+                    self.data.get("education"),
                 ]
-            ]
+            ).reshape(1, -1)
         )
 
         prediction = {}
-        prediction["binary"] = str(phone_model.predict(scaled_data)[0])
+        prediction["binary"] = str(bodycare_model.predict(scaled_data)[0])
         prediction["probability"] = str(
-            int(max(phone_model.predict_proba(scaled_data)[0]))
+            int(max(bodycare_model.predict_proba(scaled_data)[0]))
         )
         return prediction
 
@@ -130,19 +146,20 @@ class BeautyPredicter:
         if not self.proper_data():
             return 100.00
 
-        scaled_data = phone_model_scaler.transform(
-            [
+        scaled_data = bodycare_model_scaler.transform(
+            np.array(
                 [
-                    self.data.get("gender"),
                     self.data.get("age"),
-                    self.data.get("income"),
+                    self.data.get("income") // 1000,
+                    self.data.get("gender"),
+                    self.data.get("education"),
                 ]
-            ]
+            ).reshape(1, -1)
         )
 
         prediction = {}
-        prediction["binary"] = str(phone_model.predict(scaled_data)[0])
+        prediction["binary"] = str(bodycare_model.predict(scaled_data)[0])
         prediction["probability"] = str(
-            int(max(phone_model.predict_proba(scaled_data)[0]))
+            int(max(bodycare_model.predict_proba(scaled_data)[0]))
         )
         return prediction
